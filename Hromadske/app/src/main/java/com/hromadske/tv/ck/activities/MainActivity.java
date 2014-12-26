36afe1be5e98d2ebb50dce6a0ed7b9209029c58e
chebts <chebTS@gmail.com>
2014-12-27 01:02:44 +0200

    refactored

--- Hromadske/app/src/main/java/com/hromadske/tv/ck/activities/MainActivity.java
+++ Hromadske/app/src/main/java/com/hromadske/tv/ck/activities/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.widget.DrawerLayout;
 
 import com.hromadske.tv.ck.fragments.NavigationDrawerFragment;
 import com.hromadske.tv.ck.R;
+import com.hromadske.tv.ck.fragments.PlaceholderFragment;
 
 
 public class MainActivity extends ActionBarActivity
@@ -107,41 +108,6 @@ public class MainActivity extends ActionBarActivity
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
 
 }
