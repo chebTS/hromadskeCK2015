5159667f043c2d9fd17346008d49c03a739464d0
chebts <chebTS@gmail.com>
2014-12-27 00:57:20 +0200

    entitie

---
+++ Hromadske/app/src/main/java/com/hromadske/tv/ck/entities/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.hromadske.tv.ck.entities;
+
+/**
+ * Created by cheb on 12/27/14.
+ */
+public class BaseEntity {
+    private int id;
+    private String title;
+    private String introText;
+    private String fullText;
+    private long created;
+    private String video;
+    private String image;
+}
+
