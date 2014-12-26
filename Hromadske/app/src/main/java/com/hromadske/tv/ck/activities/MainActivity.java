cfe042ec905137284e314b1401cffe209df0f56a
chebts <chebTS@gmail.com>
2014-12-27 00:36:25 +0200

    structure

--- Hromadske/app/src/main/java/com/hromadske/tv/ck/activities/MainActivity.java
+++ Hromadske/app/src/main/java/com/hromadske/tv/ck/activities/MainActivity.java
@@ -1,22 +1,20 @@
-package com.hromadske.tv.ck;
+package com.hromadske.tv.ck.activities;
 
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+
+import com.hromadske.tv.ck.fragments.NavigationDrawerFragment;
+import com.hromadske.tv.ck.R;
 
 
 public class MainActivity extends ActionBarActivity
---
+++ Hromadske/app/src/main/java/com/hromadske/tv/ck/utils/SystemUtils.java
@@ -0,0 +1,26 @@
+package com.hromadske.tv.ck.utils;
+
+/**
+ * Created by cheb on 12/27/14.
+ */
+public class SystemUtils {
+        private static final String BASE_URL = "http://hromadske.cherkasy.ua/?option=com_hromadskeapi";
+        public static final String NEWS_URL = BASE_URL + "&category=news";
+        public static final String POLITICS_URL = BASE_URL + "&category=polityka";
+        public static final String SOCIETY_URL = BASE_URL + "&category=suspilstvo";
+        public static final String CULTURE_URL = BASE_URL + "&category=kultura";
+        public static final String KINO_URL = BASE_URL + "&category=kinodoc";
+        public static final String PHOTO_URL = BASE_URL + "&category=fotodoc";
+        public static final String TEAM_URL = BASE_URL + "&category=team";
+
+}
+//        http://hromadske.cherkasy.ua/?option=com_hromadskeapi&category=news
+//        http://hromadske.cherkasy.ua/?option=com_hromadskeapi&category=polityka
+//        http://hromadske.cherkasy.ua/?option=com_hromadskeapi&category=suspilstvo
+//        http://hromadske.cherkasy.ua/?option=com_hromadskeapi&category=kultura
+//        http://hromadske.cherkasy.ua/?option=com_hromadskeapi&category=kinodoc
+//        http://hromadske.cherkasy.ua/?option=com_hromadskeapi&category=fotodoc
+//        http://hromadske.cherkasy.ua/?option=com_hromadskeapi&category=team
+//
+
+
