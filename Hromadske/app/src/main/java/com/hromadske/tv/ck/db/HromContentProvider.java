f6b5f9b0887ee0fdcff8f3cd8c2b2f3dfe2ef8f9
ChebTS <chebTS@gmail.com>
2014-12-30 01:22:21 +0200

    Content provider

--- Hromadske/app/src/main/java/com/hromadske/tv/ck/db/HromContentProvider.java
+++ Hromadske/app/src/main/java/com/hromadske/tv/ck/db/HromContentProvider.java
@@ -1,12 +1,14 @@
 package com.hromadske.tv.ck.db;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.util.Log;
 
@@ -22,7 +24,7 @@ public class HromContentProvider extends ContentProvider {
     DBHelper dbHelper;
     SQLiteDatabase db;
 
-    static final String TEAM_TABLE = "team";
+    static final String TABLE_TEAM = "team";
 
     static final String TEAM_ID = "_id";
     static final String TEAM_TITLE = "title";
@@ -32,7 +34,7 @@ public class HromContentProvider extends ContentProvider {
     static final String TEAM_VIDEO = "video";
     static final String TEAM_IMAGE = "image";
 
-    static final String DB_CREATE = "create table " + TEAM_TABLE + "("
+    static final String DB_CREATE = "create table " + TABLE_TEAM + "("
             + TEAM_ID + " integer primary key autoincrement, "
             + TEAM_TITLE + " text, "
             + TEAM_INTROTEXT + " text, "
@@ -53,13 +55,13 @@ public class HromContentProvider extends ContentProvider {
     static final String CONTACT_CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd."
             + AUTHORITY + "." + TEAM_PATH;
 
-    static final int URI_TEAM = 1;
+    static final int URI_TEAM_LIST = 1;
     static final int URI_TEAM_ID = 2;
 
     private static final UriMatcher uriMatcher;
     static {
         uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-        uriMatcher.addURI(AUTHORITY, TEAM_PATH, URI_TEAM);
+        uriMatcher.addURI(AUTHORITY, TEAM_PATH, URI_TEAM_LIST);
         uriMatcher.addURI(AUTHORITY, TEAM_PATH + "/#", URI_TEAM_ID);
     }
 
@@ -72,25 +74,47 @@ public class HromContentProvider extends ContentProvider {
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
         switch (uriMatcher.match(uri)) {
-            case URI_TEAM:
-
+            case URI_TEAM_LIST:
+                qb.setTables(TABLE_TEAM);
                 break;
             case URI_TEAM_ID:
-
+                qb.setTables(TABLE_TEAM);
+                qb.appendWhere(TEAM_ID + "=" + uri.getLastPathSegment());
                 break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
         }
-        return null;
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor c = qb.query(db,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
     }
 
-    @Override
-    public String getType(Uri uri) {
-        return null;
-    }
+
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        long id;
+        switch (uriMatcher.match(uri)){
+            case URI_TEAM_LIST:
+                id = db.insert(TABLE_TEAM, null, values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported URI for insertion: " + uri);
+        }
+        Uri itemUri = ContentUris.withAppendedId(uri, id);
+        getContext().getContentResolver().notifyChange(itemUri, null);
+        return itemUri;
     }
 
     @Override
@@ -119,11 +143,36 @@ public class HromContentProvider extends ContentProvider {
                 cv.put(TEAM_CREATED, "created " + i);
                 cv.put(TEAM_VIDEO, "video " + i);
                 cv.put(TEAM_IMAGE, "image " + i);
-                db.insert(TEAM_TABLE, null, cv);
+                db.insert(TABLE_TEAM, null, cv);
             }
         }
 
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db.beginTransaction();
+        for (int i = 0; i < values.length; i++) {
+            this.insert(uri, values[i]);
+        }
+        db.setTransactionSuccessful();
+        db.endTransaction();
+        return values.length;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = uriMatcher.match(uri);
+        switch (match) {
+            case URI_TEAM_LIST:
+                return CONTACT_CONTENT_TYPE;
+            case URI_TEAM_ID:
+                return CONTACT_CONTENT_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
     }
 }
