59b4e26526a77ec76dd5224803b8077200be7f59
ChebTS <chebTS@gmail.com>
2014-12-31 12:29:40 +0200

    offline

--- Hromadske/app/src/main/java/com/hromadske/tv/ck/fragments/SocietyListFragment.java
+++ Hromadske/app/src/main/java/com/hromadske/tv/ck/fragments/SocietyListFragment.java
@@ -1,9 +1,12 @@
 package com.hromadske.tv.ck.fragments;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +18,8 @@ import com.hromadske.tv.ck.R;
 import com.hromadske.tv.ck.activities.DetailActivity;
 import com.hromadske.tv.ck.activities.MainActivity;
 import com.hromadske.tv.ck.adapters.BaseEntitiesAdapter;
+import com.hromadske.tv.ck.adapters.EntityCursorAdapter;
+import com.hromadske.tv.ck.db.HromContentProvider;
 import com.hromadske.tv.ck.entities.BaseEntity;
 import com.hromadske.tv.ck.tasks.EntitiesLoader;
 import com.hromadske.tv.ck.utils.SystemUtils;
@@ -25,11 +30,16 @@ import static com.hromadske.tv.ck.utils.SystemUtils.saveData;
 /**
  * Created by cheb on 12/27/14.
  */
-public class SocietyListFragment extends BaseMenuFragment implements AdapterView.OnItemClickListener, LoaderManager.LoaderCallbacks<RightList<BaseEntity>>  {
+public class SocietyListFragment extends BaseMenuFragment implements
+        AdapterView.OnItemClickListener,
+        LoaderManager.LoaderCallbacks<Object>  {
     private static final String TAG = CultureListFragment.class.getSimpleName();
     private ListView listView;
     static final int LOADER_ID = 2;
+    static final int CURSOR_LOADER_ID = 22;
     private View progressBar;
+    private boolean isLoading = true;
+    private EntityCursorAdapter entityCursorAdapter;
 
     public static SocietyListFragment newInstance(int sectionNumber) {
         SocietyListFragment fragment = new SocietyListFragment();
@@ -58,47 +68,88 @@ public class SocietyListFragment extends BaseMenuFragment implements AdapterView
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(getActivity(), DetailActivity.class);
-        intent.putExtra(SystemUtils.EXTRA_ENTITY, (BaseEntity)parent.getItemAtPosition(position));
-        if (((MainActivity)getActivity()).getTabletContainer() == null){
-            startActivity(intent);
+        if (isLoading) {
+            Intent intent = new Intent(getActivity(), DetailActivity.class);
+            intent.putExtra(SystemUtils.EXTRA_ENTITY, (BaseEntity) parent.getItemAtPosition(position));
+            if (((MainActivity) getActivity()).getTabletContainer() == null) {
+                startActivity(intent);
+            } else {
+                DetailFragment detailFragment = new DetailFragment();
+                detailFragment.setArguments(intent.getExtras());
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.tablet_container, detailFragment)
+                        .commit();
+            }
         }else{
-            DetailFragment detailFragment = new DetailFragment();
-            detailFragment.setArguments(intent.getExtras());
-            getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.tablet_container, detailFragment)
-                    .commit();
+            Cursor cursor = entityCursorAdapter.getCursor();
+            cursor.moveToPosition(position);
+            BaseEntity entity = SystemUtils.buildEntity(cursor);
+            Intent intent = new Intent(getActivity(), DetailActivity.class);
+            intent.putExtra(SystemUtils.EXTRA_ENTITY, entity);
+            if (((MainActivity) getActivity()).getTabletContainer() == null) {
+                startActivity(intent);
+            } else {
+                DetailFragment detailFragment = new DetailFragment();
+                detailFragment.setArguments(intent.getExtras());
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.tablet_container, detailFragment)
+                        .commit();
+            }
         }
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         getLoaderManager().initLoader(LOADER_ID, null, this);
+        getLoaderManager().initLoader(CURSOR_LOADER_ID, null, this);
         super.onActivityCreated(savedInstanceState);
         if (SystemUtils.isOnline(getActivity())) {
+            isLoading = true;
             Loader<RightList<BaseEntity>> loader = getLoaderManager().getLoader(LOADER_ID);
             loader.forceLoad();
             progressBar.setVisibility(View.VISIBLE);
         }else{
-            //TODO
+            isLoading = false;
+            Loader<Cursor> cursorLoader = getLoaderManager().getLoader(CURSOR_LOADER_ID);
+            cursorLoader.forceLoad();
+            progressBar.setVisibility(View.VISIBLE);
         }
     }
 
     @Override
-    public Loader<RightList<BaseEntity>> onCreateLoader(int id, Bundle args) {
-        Loader<RightList<BaseEntity>> loader = new EntitiesLoader(getActivity(), SystemUtils.SOCIETY_URL, progressBar);
-        return loader;
+    public Loader onCreateLoader(int id, Bundle args) {
+        switch (id){
+            case LOADER_ID:
+                EntitiesLoader loader = new EntitiesLoader(getActivity(), SystemUtils.SOCIETY_URL, progressBar);
+                return loader;
+            case CURSOR_LOADER_ID:
+                Uri CONTENT_URI = HromContentProvider.SOCIETY_CONTENT_URI;
+                return new CursorLoader(getActivity(), CONTENT_URI, null, null, null, null);
+        }
+        return null;
     }
 
     @Override
-    public void onLoadFinished(Loader<RightList<BaseEntity>> loader, RightList<BaseEntity> data) {
+    public void onLoadFinished(Loader loader, Object data) {
         progressBar.setVisibility(View.GONE);
-        listView.setAdapter(new BaseEntitiesAdapter(getActivity(), R.layout.item_entity, data));
-        saveData(getActivity(), SystemUtils.SOCIETY_URL, data);
+        switch (loader.getId()){
+            case LOADER_ID:
+                RightList<BaseEntity> baseEntities = (RightList<BaseEntity>)data;
+                listView.setAdapter(new BaseEntitiesAdapter(getActivity(), R.layout.item_entity, baseEntities));
+                saveData(getActivity(), SystemUtils.SOCIETY_URL, baseEntities);
+                break;
+            case CURSOR_LOADER_ID:
+                if (!isLoading) {
+                    Cursor cursor = (Cursor) data;
+                    entityCursorAdapter = new EntityCursorAdapter(getActivity(), cursor, 0);
+                    listView.setAdapter(entityCursorAdapter);
+                }
+                break;
+        }
     }
 
     @Override
-    public void onLoaderReset(Loader<RightList<BaseEntity>> loader) {
+    public void onLoaderReset(Loader loader) {
 
     }
 }
