fc6b1a3b443cf12bc4f3a4c5716344f2113c0e44
ChebTS <chebTS@gmail.com>
2014-12-31 13:58:25 +0200

    refactored

--- Hromadske/app/src/main/java/com/hromadske/tv/ck/utils/SystemUtils.java
+++ Hromadske/app/src/main/java/com/hromadske/tv/ck/utils/SystemUtils.java
@@ -76,20 +76,6 @@ public class SystemUtils {
         cVVector.toArray(cvArray);
         context.getContentResolver().bulkInsert(getURIbyURL(url), cvArray);
 
-/*
-        if (url.equals(SystemUtils.POLITICS_URL)){
-            context.getContentResolver().bulkInsert(HromContentProvider.POLITICS_CONTENT_URI, cvArray);
-        }else if (url.equals(SystemUtils.SOCIETY_URL)){
-            context.getContentResolver().bulkInsert(HromContentProvider.SOCIETY_CONTENT_URI, cvArray);
-        }else if (url.equals(SystemUtils.CULTURE_URL)){
-            context.getContentResolver().bulkInsert(HromContentProvider.CULTURE_CONTENT_URI, cvArray);
-        }else if (url.equals(SystemUtils.FILMS_URL)){
-            context.getContentResolver().bulkInsert(HromContentProvider.FILMS_CONTENT_URI, cvArray);
-        }else if (url.equals(SystemUtils.PHOTOES_URL)){
-            context.getContentResolver().bulkInsert(HromContentProvider.PHOTOES_CONTENT_URI, cvArray);
-        }else if (url.equals(SystemUtils.TEAM_URL)){
-            context.getContentResolver().bulkInsert(HromContentProvider.TEAM_CONTENT_URI, cvArray);
-        }*/
     }
 
     public static Uri getURIbyURL(String url){
